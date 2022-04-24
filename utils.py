e4918170d6ef27cc2b247df15e9a5541ca0e0ab1
Alicia-Sheng <duowensheng@gmail.com>
2022-04-24 16:59:04 -0400

    Preprocessed data in utils

--- utils.py
+++ utils.py
@@ -14,9 +14,42 @@ def load_clean_wapo_with_embedding(
     f = open(wapo_jl_path)
     data = json.load(f)
     for d in data:
-        
-        yield d
+        doc = dict()
+        doc["id"] = d["id"]
+        doc["title"] = d["title"]
+        instruction = []
+        for instruction_line in d["instructions"]:
+            instruction.append(instruction_line["text"])
+        doc["instructions"] = instruction
+        doc["nutr_lights_per100g"] = d["fsa_lights_per100g"]
+        doc["ingredients"] = dict()
+        for i in range(len(d["ingredients"])):
+            ingredient = d["ingredients"][i]
+            text = ingredient["text"]
+            texts = text.split(",")
+            ing = texts[0].strip()
+            doc["ingredients"][ing] = dict()
+            des = ""
+            for j in range(1, len(texts)):
+                t = texts[j]
+                if j == len(texts) - 1:
+                    des += t.strip()
+                else:
+                    des += (t.strip() + ", ")
+            doc["ingredients"][ing]["description"] = des
+            quantity = d["quantity"][i]
+            unit = d["unit"][i]
+            q_u = quantity["text"] + " " + unit["text"]
+            doc["ingredients"][ing]["quantity"] = q_u
+            nutr = d["nutr_per_ingredient"][i]
+            doc["ingredients"][ing]["nutr_per_ingredient"] = nutr
+        doc["nutr_values_per100g"] = d["nutr_values_per100g"]
+        doc["url"] = d["url"]
+        yield doc
+        # print(doc)
+        # break
 
 
 if __name__ == "__main__":
+    # load_clean_wapo_with_embedding("data/recipes_with_nutritional_info.json")
     pass
