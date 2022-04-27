33f7efb14820a9fd5627a3b07429f45b9b012575
Alicia-Sheng <duowensheng@gmail.com>
2022-04-27 16:38:59 -0400

    Connect search to app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import ast
 from flask import Flask, render_template, request
 from typing import List, Dict, Tuple
+from typing import List
+from elasticsearch_dsl import Search
+from elasticsearch_dsl.query import Match, MatchAll, ScriptScore, Ids, Query
+from elasticsearch_dsl.connections import connections
 
 app = Flask(__name__)
+index_name= "cooking_recipe"
+top_k = 20
+ONE_PAGE = 8 # maximum number of snippets on one page
+response = []
+docs = {}
 
 # home page
 @app.route("/")
@@ -17,10 +26,30 @@ def home():
 # result page
 @app.route("/results", methods=["POST"])
 def results():
+    global response
+    connections.create_connection(hosts=["localhost"], timeout=100, alias="default")
     query_text = request.form["main_query"]
     fat = request.form["fat"]
-    return render_template("results.html", query=query_text, fat=fat)
+    q_title = Match(title={"query": query_text})
+    # q_instructions = Match(instructions={"query": "Layer"})
+    response = search_by_healthiness(index_name, q_title, top_k)
+    temp_result = response[:ONE_PAGE]
+    return render_template("results_test.html", query=query_text, fat=fat, doc=temp_result)
+    # return render_template("results.html", query=query_text, fat=fat, doc=temp_result)
 
+def search(index: str, query: Query, top_k) -> None:
+    s = Search(using="default", index=index).query(query)[:top_k]  # initialize a query and return top five results
+    r = s.execute()
+    return r
+
+def search_by_healthiness(index: str, query: Query, top_k) -> None:
+    s = Search(using="default", index=index).query(query)[:top_k].sort({"healthiness": {"order": "desc"}})  # initialize a query and return top five results
+    r = s.execute()
+    return r
+    # for hit in r:
+    #     print(
+    #         hit.meta.id, hit.meta.score, hit.title, hit.ingredients, hit.healthiness, sep="\t"
+    #     )  # print the document id that is assigned by ES index, score and title
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
