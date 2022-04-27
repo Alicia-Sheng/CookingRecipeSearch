4367afd80395b03e52b60cb9b8a6903cd43b594e
GuangyaoDou <gordondou0330@gmail.com>
2022-04-27 15:32:23 -0400

    added testing files

---
+++ examples/example_query.py
@@ -0,0 +1,85 @@
+from typing import List
+
+from elasticsearch_dsl import Search
+from elasticsearch_dsl.query import Match, MatchAll, ScriptScore, Ids, Query
+from elasticsearch_dsl.connections import connections
+from embedding_service.client import EmbeddingClient
+
+# encoder = EmbeddingClient(host="localhost", embedding_type="sbert")
+# encoder = EmbeddingClient(host="localhost", embedding_type="fasttext")
+
+def generate_script_score_query(query_vector: List[float], vector_name: str) -> Query:
+    """
+    generate an ES query that match all documents based on the cosine similarity
+    :param query_vector: query embedding from the encoder
+    :param vector_name: embedding type, should match the field name defined in BaseDoc ("ft_vector" or "sbert_vector")
+    :return: an query object
+    """
+    q_script = ScriptScore(
+        # query={"match_all": {}},  # use a match-all query
+        query={"match_all": {}},  # use a match-all query
+        script={  # script your scoring function
+            "source": f"cosineSimilarity(params.query_vector, '{vector_name}') + 1.0",
+            # "source": f"_score",
+            # add 1.0 to avoid negative score
+            "params": {"query_vector": query_vector},
+        },
+    )
+    return q_script
+
+def search(index: str, query: Query) -> None:
+    s = Search(using="default", index=index).query(query)[
+        :5
+    ]  # initialize a query and return top five results
+    response = s.execute()
+    for hit in response:
+        print(
+            hit.meta.id, hit.meta.score, hit.title, hit.ingredients ,sep="\t"
+        )  # print the document id that is assigned by ES index, score and title
+
+
+def rescore_search(index: str, query: Query, rescore_query: Query) -> None:
+    s = Search(using="default", index=index).query(query)[
+        :5
+    ]  # initialize a query and return top five results
+    s = s.extra(
+        rescore={
+            "window_size": 5,
+            "query": {
+                "rescore_query": rescore_query,
+                "query_weight": 0,
+                "rescore_query_weight": 1,
+            },
+        }
+    )
+    response = s.execute()
+    for hit in response:
+        print(
+            hit.meta.id, hit.meta.score, hit.title, sep="\t"
+        )  # print the document id that is assigned by ES index, score and title
+
+
+if __name__ == "__main__":
+    # python load_es_index.py --index_name recipe_data --path data/recipes_with_nutritional_info.json
+    connections.create_connection(hosts=["localhost"], timeout=100, alias="default")
+
+    q_match_all = MatchAll()  # a query that matches all documents
+    q_title = Match(
+        title={"query": "chicken"}
+    )  # a query that matches "D.C" in the title field of the index, using BM25 as default
+
+    q_instructions = Match(
+        instructions={"query": "Layer"}
+    )  # a query that matches "D.C" in the title field of the index, using BM25 as default
+
+    search(
+        "recipe_data", q_title
+    )  # search, change the query object to see different results
+
+    print("="*20)
+
+    search(
+        "recipe_data", q_instructions
+    )  # search, change the query object to see different results
+
+    print("="*20)
